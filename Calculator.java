be091ef070dc08c5e8ac839e2d6cb6d16af1f074
rahulz9897 <107829751+rahulz9897@users.noreply.github.com>
2022-06-21 11:26:16 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,16 +1,8 @@
 
-//31.	Create a class MathOperation with two data member X and Y to store the operand and
-//third data member R to store result of operation.
-//Create method members
-//●	init - to input X and Y from user
-//●	add - to add X and Y and store in R
-//●	multiply - to multiply X and Y and store in R
-//●	power - to calculate X Y and store in R
-//●	display- to display Result R
 
 import java.util.Scanner;
 
-public class Mathoperation {
+public class AdvanceCalculator {
 	
 	private int X,Y,R;
 	
