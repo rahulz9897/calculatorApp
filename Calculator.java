428a70c09eda206ec7abb2c48da60baffc83e86c
rahulz9897 <107829751+rahulz9897@users.noreply.github.com>
2022-06-29 10:56:36 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -47,31 +47,31 @@ public class Calculator {
 		int x = 5;
 		int y = 4;
 		m1.init(x, y);
-		do{
-			System.out.println("Enter Your Choice\n1.Addition 2.Multiplication 3.Power 0.Exit");
-			n = s.nextInt();
-			switch(n)
-			{
-			case 0:
-				n=0;
-				break;
-			case 1:
+		//do{
+			//System.out.println("Enter Your Choice\n1.Addition 2.Multiplication 3.Power 0.Exit");
+			//n = s.nextInt();
+			//switch(n)
+			//{
+			//case 0:
+			//	n=0;
+			//	break;
+			//case 1:
 				m1.add();
 				m1.display();
-				break;
-			case 2:
+			//	break;
+			//case 2:
 				m1.multiply();
 				m1.display();
-				break;
-			case 3:
+			//	break;
+			//case 3:
 				m1.power();
 				m1.display();
-				break;
+			//	break;
 				
-			default:
-				System.out.println("Invalid Input");
-			}
-		}while(n!=0);
+			//default:
+			//	System.out.println("Invalid Input");
+			//}
+		//}while(n!=0);
 		
 	}
 }
