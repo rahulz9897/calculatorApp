5bce89dacc5f1d75d63431fb857ea5e1019efb2f
rahulz9897 <107829751+rahulz9897@users.noreply.github.com>
2022-07-01 09:39:22 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -57,17 +57,17 @@ public class Calculator {
 			//	break;
 			//case 1:
 				m1.add();
-				System.out.println("Addition of "x" & "y" = " );
+				System.out.println("Addition of 5 & 4 = ");
 				m1.display();
 			//	break;
 			//case 2:
 				m1.multiply();
-				System.out.println("Multiply of "x" & "y" = " );
+				System.out.println("Multiply of 5 & 4= ");
 				m1.display();
 			//	break;
 			//case 3:
 				m1.power();
-				System.out.println("Power of "x" & "y" = " );
+				System.out.println("Power of 5 ^ 4 = ");
 				m1.display();
 			//	break;
 				
